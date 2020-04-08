7f2178fa8e7db0f97ddd014204dfac03e2f44b03
Cristina Rivera <crivera@thoughtworks.com>
2020-04-08 18:10:07 -0400

    [@crgp1] First Unity commit :)

---
+++ MyFirstGame/Assets/Scripts/Cannon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        // transform.Translate(2 * Time.deltaTime, 0, 0);
+        transform.Rotate(verticalInput * 45 * Time.deltaTime, 0, 0);
+    }
+}
